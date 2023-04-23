67afc3a4c9fc20fe81b03f8e9259c1c14a5c4d08
DynaJoshy <99937616+DynaJoshy@users.noreply.github.com>
2023-04-23 21:44:17 +0530

    Add files via upload

---
+++ arithematic_operations.rb
@@ -0,0 +1,13 @@
+a=gets.chomp.to_i
+b=gets.to_i
+puts(a+b) #additon
+puts(a-b) #subtraction
+puts(a/b) #multiplication
+puts(a*b) #division
+
+name=gets.chomp
+age=gets.chomp
+#string interpolation
+p "hello #{name},my age is #{age}"
+
+
